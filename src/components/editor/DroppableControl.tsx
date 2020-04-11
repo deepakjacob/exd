8b95efde148cd253cd7739ad243dab5e77879e08
Deepak Jacob <deepak.jacob@gmail.com>
2020-04-11 18:34:00 -0400

    refactor: added component min width warning

--- src/components/editor/DroppableControl.tsx
+++ src/components/editor/DroppableControl.tsx
@@ -1,19 +1,40 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { useDrop } from 'react-dnd';
 import { connect } from 'react-redux';
 
+import Snackbar from '@material-ui/core/Snackbar';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import MuiAlert from '@material-ui/lab/Alert';
 
 import { addControl } from '../../store/actions/allControls';
 import { ControlDesignDisplayProps, DraggableType } from '../../types';
 
-export function moveControl(control: ControlDesignDisplayProps, row: number, col: number, addControl: any) {
+const calcControlWidth = (cdp: ControlDesignDisplayProps): number => {
+  const width: any = cdp.overriden?.dimension?.width ? cdp.overriden.dimension.width : cdp.metadata.dimension.width;
+  return parseInt(width);
+};
+
+function Alert(props: any) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+export function moveControl(
+  control: ControlDesignDisplayProps,
+  row: number,
+  col: number,
+  addControl: any,
+  setOpen: (open: boolean) => void
+) {
   const controlDesignDisplayProps = control;
+  const canFit = calcControlWidth(control) + col - 1 < 12;
+  if (!canFit) {
+    setOpen(true);
+    return null;
+  }
   controlDesignDisplayProps.gridPosition = {
     row,
     col,
   };
-
   addControl(controlDesignDisplayProps);
 }
 
@@ -36,14 +57,17 @@ interface DroppableControlProps {
   col: number;
   children?: any;
 }
+
 const DroppableControl: FC<DroppableControlProps> = (props: any) => {
+  const [open, setOpen] = useState(false);
   const { row, col, children } = props;
   const classes = useStyles();
+
   const [{ isOver, canDrop }, drop] = useDrop({
     accept: DraggableType.CONTROL,
     drop: (item, monitor: any) => {
       const { control } = monitor.getItem();
-      return moveControl(control, row, col, props.addControl);
+      return moveControl(control, row, col, props.addControl, setOpen);
     },
     canDrop: () => canMoveControl(row, col),
     collect: (monitor) => ({
@@ -51,6 +75,15 @@ const DroppableControl: FC<DroppableControlProps> = (props: any) => {
       canDrop: !!monitor.canDrop(),
     }),
   });
+
+  const handleClose = (event: any, reason: any) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
     <div
       className={isOver && canDrop ? classes.highlightedItem : classes.item}
@@ -62,6 +95,11 @@ const DroppableControl: FC<DroppableControlProps> = (props: any) => {
       }}
     >
       {children}
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error">
+          Control cannot be placed as minimum width for component cannot be accommodated in available space
+        </Alert>
+      </Snackbar>
     </div>
   );
 };
